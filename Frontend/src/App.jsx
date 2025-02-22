e1bb2856d6dbd47b7ab7e342dbecefc581abfc09
Rupali Kumari <rupalik.ug23.ma@nitp.ac.in>
2025-02-22 11:30:20 +0530

    some minor fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
